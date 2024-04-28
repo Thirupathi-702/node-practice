529be1473ea44a405ef3d3a210e5d5cc698b632c
Thirupathi-702 <21r05a6702@gmail.com>
2024-04-28 17:39:21 +0530

    adding views1

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,8 @@
 module.exports.profile=function(req,res){
-    return res.end("<h1>hlo from users profile</h1>")
+    return res.render('user',{
+        title:"userpage",
+        user:'thiru'
+    })
 
 }
 module.exports.profilepoints=function(req,res){
