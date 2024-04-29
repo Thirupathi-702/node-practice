104303726500e58e3bc753a5252694218b5c62ee
Thirupathi-702 <21r05a6702@gmail.com>
2024-04-29 19:55:25 +0530

    commit

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-const express=require('express')
+const express=require('express');
+const expressEjsLayouts = require('express-ejs-layouts');
+const db=require('./config/mongoose');
 const app=express();
 const port=8000;
 app.set('view engine','ejs');
 app.set('views','./views');
+
+app.use(expressEjsLayouts)
 
 app.use('/',require('./routes'));
 app.listen(port,function(err){
