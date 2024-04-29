64ffa4656bfac22079ec6ebdf78713b6a2bddeb5
Thirupathi-702 <21r05a6702@gmail.com>
2024-04-29 22:03:21 +0530

    adding user sign in

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -8,3 +8,28 @@ module.exports.profile=function(req,res){
 module.exports.profilepoints=function(req,res){
     return res.end("<h1>Total points: 110</h1>")
 }
+
+module.exports.signUp=function(req,res){
+    return res.render('user_sign_up',{
+        title:"codial | signup"
+    })
+}
+module.exports.signUp=function(req,res){
+    return res.render('sign_up',{
+        title:"codial | sign Up"
+    })
+}
+
+module.exports.signIn=function(req,res){
+    return res.render('user_sign_in',{
+        title:"codial | sign In"
+    })
+}
+
+module.exports.create=function(req,res){
+    // todo later
+}
+
+module.exports.createSession=function(req,res){
+    //later
+}
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,10 @@
 const express=require('express');
 const router=express.Router();
 const usercontroller=require('../controllers/users_controller');
+
 router.get('/profile',usercontroller.profile);
 router.get('/points',usercontroller.profilepoints);
+router.get('/sign-up',usercontroller.signUp);
+router.get('/sign-in',usercontroller.signIn);
+
 module.exports=router;
